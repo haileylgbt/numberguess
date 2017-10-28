78e464048d142cfe66b9280fb9b4a65c7c82eb54
harrycode <pacboy2004@gmail.com>
2017-10-28 17:24:41 +1000

    there

--- learning.js
+++ learning.js
@@ -1,6 +1,6 @@
 var numberThoughtOf = 0;
 var answer = 0;
-var rememberRangeChance = 1%;
+var rememberRangeChance = 99;
 var numberLow = 0;
 var numberHigh = 0;
 var remembersRange = false;
@@ -9,10 +9,10 @@ function rand(r1, r2) {
   return Math.floor(Math.random() * r2) + r1;
 }
 
-function setNumber(n, l, h) {
+function setNumber(n) {
   numberThoughtOf = n;
-  numberLow = l;
-  numberHigh = h;
+  numberLow = n - rand(5, 10);
+  numberHigh = n + rand(5, 10);
 }
 
 function stupidGuess() {
@@ -20,10 +20,30 @@ function stupidGuess() {
 }
 
 function attemptToRemember() {
-  var remember = rand(1, (100 / rememberRangeChance);
-  if remember =
+  var remember = rand(1, (100 - rememberRangeChance));
+  if (remember === 1) {
+    remembersRange = true;
+  } else {
+    remembersRange = false;
+  }
 }
 
 function guess() {
+  if (remembersRange) {
+    answer = rand(numberLow, numberHigh);
+  } else if (!remembersRange) {
+    stupidGuess();
+  }
+}
 
+function learn(n) {
+  rememberRangeChance += n;
+}
+
+function checkAnswer() {
+  if (answer === numberThoughtOf) {
+    return true;
+  } else {
+    return false;
+  }
 }
---
+++ main.js
@@ -0,0 +1,40 @@
+var hasNotWon = true;
+
+function popup(text) {
+  alert(text);
+}
+
+function introduction() {
+  popup("Harry: Hey H4RR.Y")
+  popup(`H4RR.Y: YES, HARRY?`)
+  popup(`Harry: Wanna play a guessing game?`)
+  popup(`H4RR.Y: I DO NOT HAVE THE GUESSING MODULE INSTALLED, SO I'M AFRAID NOT.`)
+  popup(`Harry: But you do have that new machine learning module, right?`)
+  popup(`H4RR.Y: AFFIRMITIVE.`)
+  popup(`Harry: Well, that's all you need to play!`)
+  popup(`H4RR.Y: OK, BRING IT ON!`)
+  popup(`Harry: That's the spirit!`)
+  popup(`Harry: So, I'm going to pick a number-`)
+  popup(`H4ARR.Y: BETWEEN 2 NUMBERS AND I HAVE TO GUESS IT?`)
+  popup(`Harry: Yep, that's all there is to it!`)
+  popup(`H4RR.Y: OK, LET'S DO THIS!`)
+  popup(`Click on one of the game buttons below to start!`)
+}
+
+function oneGame() {
+  hasNotWon = true;
+  setNumber(rand(1, 100));
+  popup(`Harry: I'm thinking of a number between ${numberLow} and ${numberHigh}.`)
+  while (hasNotWon) {
+    attemptToRemember()
+    guess()
+    popup(`H4RR.Y: ${answer}?`)
+    if (checkAnswer()) {
+      popup(`Harry: You're right! I was thinking of ${answer}!`)
+      learn(rand(1, 2))
+      hasNotWon = false;
+    } else {
+      popup(`Harry: Nope.`)
+    }
+  }
+}
