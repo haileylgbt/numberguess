537c0beeb92e9c7dec2f1d512c85cc34e0452156
Harry <pacboy2004@gmail.com>
2017-10-28 16:20:59 +1000

    Add files via upload

---
+++ learning.js
@@ -0,0 +1,29 @@
+var numberThoughtOf = 0;
+var answer = 0;
+var rememberRangeChance = 1%;
+var numberLow = 0;
+var numberHigh = 0;
+var remembersRange = false;
+
+function rand(r1, r2) {
+  return Math.floor(Math.random() * r2) + r1;
+}
+
+function setNumber(n, l, h) {
+  numberThoughtOf = n;
+  numberLow = l;
+  numberHigh = h;
+}
+
+function stupidGuess() {
+  answer = rand(1, 100);
+}
+
+function attemptToRemember() {
+  var remember = rand(1, (100 / rememberRangeChance);
+  if remember =
+}
+
+function guess() {
+
+}
